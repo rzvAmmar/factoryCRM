3dc1080f36813ee6ab6ce1dc58a2a632eb562515
Ammar Rizvi <ammarr@vmware.com>
2016-10-04 01:32:33 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
+
+var app = express();
+
+var port = process.env.PORT || 5000;
+
+var nav = [
+    {
+        Link: '/',
+        Text: 'Home'
+              }
+];
+
+//var bookRouter = require('./src/routes/bookRoutes')(nav);
+//var adminRouter = require('./src/routes/adminRoutes')(nav);
+//var authRouter = require('./src/routes/authRoutes')(nav);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'factory'}));
+
+require('./src/config/passport')(app);
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
+//app.use('/books', bookRouter);
+//app.use('/admin', adminRouter);
+//app.use('/auth', authRouter);
+
+app.get('/', function (request, response) {
+    response.render('index', {
+        title: 'Hello from render',
+        nav: nav
+    });
+});
+
+//app.get('/books', function (request, response) {
+//    response.send('Hello Books!');
+//});
+
+app.listen(port, function (err) {
+    console.log('Running server on port ' + port);
+});
